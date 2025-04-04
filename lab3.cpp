f1492e8973d4be783602e5921efc1f76367694e0
ba40k <143427365+ba40k@users.noreply.github.com>
2025-04-04 20:29:03 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,63 +1,69 @@
 #include <iostream>
+#include <cstdint>
 using namespace std;
 
 int main() {
-    // Task 1: Compute a^12 + a^8 + a
+    
     cout << "=== TASK 1: Computing a^12 + a^8 + a ===" << endl;
-    unsigned int a;
+    uint32_t a;
     cout << "Enter positive integer a: ";
     cin >> a;
 
-    unsigned long long res1 = 0;
+    uint64_t res1 = 0; // насколько я знаю лонг лонг модет меняться в зависимости от битности системы
     __asm {
         mov eax, a
 
-       
-        mov ecx, eax; a
+        mov ecx, eax; считаем a в 12
         imul ecx, ecx; a ^ 2
         imul ecx, eax; a ^ 3
         imul ecx, ecx; a ^ 6
         imul ecx, ecx; a ^ 12
         mov ebx, ecx
 
-        ; Compute a ^ 8
-        mov ecx, eax; a
+        
+        mov ecx, eax; ну тут тоже самое но для а в 8
         imul ecx, ecx; a ^ 2
         imul ecx, ecx; a ^ 4
         imul ecx, ecx; a ^ 8
 
-        
         add ebx, ecx; a ^ 12 + a ^ 8
         add ebx, eax; +a
 
-        ; Store 64 - bit result
+        
         mov dword ptr[res1], ebx
         mov dword ptr[res1 + 4], 0
     }
-    cout << "Result: " << res1 << endl << endl;
+    cout << "Result: " << res1 << "\n\n";
 
-
-
-    // Task 2: Find integer solutions to 2x + 3y = 50
+   
     cout << "=== TASK 2: Finding solutions to 2x + 3y = 50 ===" << endl;
+
+    const int MAX_SOLUTIONS = 51;  // логично что если числа положительные то ответов максимум 51 (спойлер - 8)
+    int solutionsX[MAX_SOLUTIONS];
+    int solutionsY[MAX_SOLUTIONS];
     int cnt = 0;
-
-    for (int x = 1; x <= 50; x++) {
-        int y = 0;
-
-
-
+    int x;
+    __asm {
+        mov x, 1; x начинается с 1
+        jmp start_loop
 
 
-        
-        int valid = 0;
 
-        __asm {
-            mov eax, 50
+
+
+        loop_body :
+       
+        mov eax, 50; 50 которое ответ, а не 51 которое макс число решений
+            
+            
+            
             mov ebx, x
             imul ebx, 2
-            sub eax, ebx
+            sub eax, ebx; eax = 50 - 2x
 
+            ; Проверяем, что(50 - 2x) > 0 и делится на 3 без остатка
+
+           
             cmp eax, 0
             jle not_valid
 
@@ -68,23 +74,38 @@ int main() {
             cmp edx, 0
             jne not_valid
 
-            mov y, eax
+            
+            cmp eax, 0
+            jle not_valid
 
             
-            mov valid, 1
+            mov edi, cnt
+            shl edi, 2
+            mov ebx, x
+            mov solutionsX[edi], ebx
+            mov solutionsY[edi], eax
+
+            
+            inc cnt
 
             not_valid :
-        }
-        if (y <= 0 || x <= 0) {
-            valid = 0;
-        }
-        if (valid ) {
-            cnt++;
-            cout << "Solution found: 2*" << x << " + 3*" << y << " = 50" << endl;
-        }
+        inc x
+
+            start_loop:
+        cmp x, 51
+            jle loop_body
     }
 
-    cout << "Total solutions found: " << cnt << endl;
+    
+    if (cnt > 0) {
+        cout << "Found solutions:" << endl;
+        for (int i = 0; i < cnt; i++) {
+            cout << "2*" << solutionsX[i] << " + 3*" << solutionsY[i] << " = 50" << endl;
+        }
+    }
+    else {
+        cout << "No solutions found.\n";
+    }
+    cout << "Total solutions found: " << cnt << '\n';
 
-    return 0;
 }
